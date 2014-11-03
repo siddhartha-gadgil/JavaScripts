c74894b83eb08363013cb77cd161d4a18d9a60ff
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2014-11-03 06:43:37 +0530

    plotSeq

--- ConditionalConvergence/conditionalconvergence.js
+++ ConditionalConvergence/conditionalconvergence.js
@@ -4,31 +4,43 @@ angular.module('CondConv',[])
         return Math.abs(target - sum - element) < Math.abs(target - sum)
       }
       function approxSeq(target, seq, error) {
-        context.clearRect(0, 0, Width, Height);
-        drawLine(0, 0, Width, 0, "black");
-          drawLine(0, target, Width, target, "red");
         var accum = [];
         var partialsum = 0.0;
         for (var n = 1.0; Math.abs(target - partialsum) > error; n++) {
           if (include(partialsum, target, seq(n))) {
             accum.push(seq(n));
             partialsum += seq(n);
-            drawLine(n * $scope.spacing, 0, n * $scope.spacing, partialsum, "blue");
           }
         }
         return accum;
       }
 
-      example = function (n) {
+      var example = function (n) {
         if (n % 2 == 1) {
           return (-1.0) / n;
         } else return 1.0 / n;
       };
 
-      oneovern = function (x) {
+      $scope.fntext = "n %2 == 1 ? -1 / n : 1 /n";
+
+      $scope.seqtext = $scope.fntext;
+
+      $scope.updatefmla = function(){
+          $scope.seqtext = $scope.fntext;
+          rawprint = true;
+      };
+
+      function parsefn(txt){
+          return new Function('n', "return " + txt);
+      }
+
+      var rawprint = false;
+
+        function oneovern (x) {
+          if (rawprint) return x.toString();
         var d = Math.round(1 / x);
         if (d < 0) return "(-1/" + (-d).toString() + ")"; else return "(1/" + d.toString() + ")";
-      };
+      }
 
       function printSeq(seq) {
         var sum = seq.reduce(function (a, b) {
@@ -39,13 +51,30 @@ angular.module('CondConv',[])
               return a + " + " + b;
             }, '');
       }
+
+
+      function plotSeq(seq, target){
+          context.clearRect(0, 0, Width, Height);
+          drawLine(0, 0, Width, 0, "black");
+          drawLine(0, target, Width, target, "red");
+          var partialsum = 0.0;
+          var spc = $scope.spacing;
+          var x = 1;
+          seq.forEach(function(y){
+              partialsum += y;
+              drawLine($scope.spacing * x, 0, $scope.spacing * x, partialsum, "blue");
+              x += 1;
+          })
+      }
+
       $scope.accuexp = 1.0;
 
       $scope.result =function () {
         var targ = parseFloat($scope.target);
         var accu = Math.pow(10.0, - parseFloat($scope.accuexp)/10.0);
-  //      return accu.toString();
-        var out =  printSeq( approxSeq(targ, example,  accu));
+        var approx = approxSeq(targ, parsefn($scope.seqtext),  accu);
+        var out =  printSeq(approx);
+          plotSeq(approx, targ);
         return out;
       };
 
